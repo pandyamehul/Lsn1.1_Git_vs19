ef3c66af642be7df1aa03fbaafcf7501759459b0
pandyamehul <pandya.mehul@live.com>
2020-05-16 12:13:08 +0200

    Ch#1 - Initial Commit, start of project / base branch.

---
+++ ConApp_Lsn1.1_Git_vs19/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConApp_Lsn1._1_Git_vs19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
